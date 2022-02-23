0996ac94e0e48ec19b757e1a1e11f47de4d2e94d
Nuwan-alt <jayasankanuwan1998@gmail.com>
2022-02-23 23:30:06 +0530

    updated

--- dbCon.php
+++ dbCon.php
@@ -1,7 +1,7 @@
 <?php
 $servername='localhost';
-$username='root';
-$password='';
+$username='uomleoso_root';
+$password='bestleos2017';
 $dbname = "debatinators 22'";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
