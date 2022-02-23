b0413ca25e50db9df9a243032eec2c07491c9ad9
Nuwan-alt <jayasankanuwan1998@gmail.com>
2022-02-23 23:41:12 +0530

    made table

--- dbCon.php
+++ dbCon.php
@@ -2,7 +2,7 @@
 $servername='localhost';
 $username='uomleoso_root';
 $password='bestleos2017';
-$dbname = "debatinators 22'";
+$dbname = "uomleoso_debatinators";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 ?>
